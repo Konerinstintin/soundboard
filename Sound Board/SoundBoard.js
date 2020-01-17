0f45a71360b7cab02f88a89cda9e5fc02eea1fb8
Konerinstintin <38484296+Konerinstintin@users.noreply.github.com>
2020-01-17 16:47:12 -0500

    Add files via upload

---
+++ Sound Board/SoundBoard.js
@@ -0,0 +1,23 @@
+function button1() {
+    document.getElementById('audiotag1').play();
+}
+
+function button2() {
+    document.getElementById('audiotag2').play();
+}
+
+function button3() {
+    document.getElementById('audiotag3').play();
+}
+
+function button4() {
+    document.getElementById('audiotag4').play();
+}
+
+function button5() {
+    document.getElementById('audiotag5').play();
+}
+
+function button6() {
+    document.getElementById('audiotag6').play();
+}
